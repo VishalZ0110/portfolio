aa1079f17dd26fde4e9951277023000b18f02622
VishalZ0110 <vishalvy789@gmail.com>
2026-03-24 12:39:47 +0530

    Removes blog post component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
-import BlogPost from './pages/BlogPost';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
       </Routes>
     </>
   );
